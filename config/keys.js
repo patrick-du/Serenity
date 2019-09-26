e24c84753a1ff8e5687e8a687b424b2126598da6
Patrick Du <patrickduu@gmail.com>
2019-09-26 16:37:19 -0400

    server setup and user model

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://admin:Serenity123@serenitydb-urjqs.mongodb.net/test?retryWrites=true&w=majority"
+}
+
---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = User = mongoose.model('User', UserSchema);
+
+// How the export works with mongoose.model: https://mongoosejs.com/docs/api.html#model_Model
