f7c1ba4de7eccd1be4a648dc81cc4ca0d3897a86
Patrick Du <patrickduu@gmail.com>
2019-10-09 12:09:59 -0400

    splash screen

--- client/App.js
+++ client/App.js
@@ -18,6 +18,17 @@ export default class App extends Component {
 
   async componentWillMount() {
     await Font.loadAsync({
+      'OpenSans-Light': require('./assets/fonts/OpenSans-Light.ttf'),
+      'OpenSans-Regular': require('./assets/fonts/OpenSans-Regular.ttf'),
+      'OpenSans-SemiBold': require('./assets/fonts/OpenSans-SemiBold.ttf'),
+      'OpenSans-Bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+
+      'Roboto-Thin': require('./assets/fonts/Roboto-Thin.ttf'),
+      'Roboto-Light': require('./assets/fonts/Roboto-Light.ttf'),
+      'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
+      'Roboto-Medium': require('./assets/fonts/Roboto-Medium.ttf'),
+      'Roboto-Bold': require('./assets/fonts/Roboto-Bold.ttf'),
+
       'Rubik-Black': require('./node_modules/@shoutem/ui/fonts/Rubik-Black.ttf'),
       'Rubik-Bold': require('./node_modules/@shoutem/ui/fonts/Rubik-Bold.ttf'),
       'Rubik-Light': require('./node_modules/@shoutem/ui/fonts/Rubik-Light.ttf'),
--- client/components/Auth/Welcome.js
+++ client/components/Auth/Welcome.js
@@ -1,24 +1,28 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 
 export default class WelcomeScreen extends Component {
 
   onPressSignUp = () => {
-    this.props.navigation.navigate('SignUp')  
+    this.props.navigation.navigate('SignUp')
   };
 
   onPressLogin = () => {
-    this.props.navigation.navigate('Login')  
+    this.props.navigation.navigate('Login')
   };
-  
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>
-          <Text onPress={this.onPressLogin}>Login</Text>
-          <Text style={styles.currentTab} onPress={this.onPressSignUp}>Sign Up</Text>
-        </Text>
-      </View>
+        <View style={styles.container}>
+          <Text style={styles.logo}>SerenityLogo</Text>
+          <Text style={styles.title}>Let's get started!</Text>
+          <Text style={styles.subtitle}>Create an account to continue. We don't sell or share your data with anyone.</Text>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText} onPress={this.onPressSignUp}>Log in</Text>
+          </TouchableOpacity>
+          <View style={styles.lineStyle}/>
+          <Text style={styles.subtitle}>You don't have an account? <Text style={{color: '#778beb', fontFamily: 'OpenSans-SemiBold'}} onPress={this.onPressLogin}>Sign up here</Text></Text>
+        </View>
     );
   }
 }
@@ -26,11 +30,48 @@ export default class WelcomeScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 60,
-    backgroundColor: '#23272A',
+    paddingVertical: 60,
+    paddingHorizontal: 50,
+    backgroundColor: '#F6F8FA', // basically white colour
   },
-  currentTab: {
-    fontWeight: 'bold',
-    textDecorationLine: 'underline'
-  }
-});
+  logo: {
+    textAlign: 'center',
+    fontFamily: 'OpenSans-SemiBold',
+    fontSize: 30,
+    color: '#5C5B6F', // dark navy colour
+    marginTop: 80
+  },
+  title: {
+    textAlign: 'center',
+    fontFamily: 'OpenSans-SemiBold',
+    fontSize: 30,
+    color: '#5C5B6F', // dark navy colour
+    marginTop: 80,
+    marginBottom: 15,
+  },
+  subtitle: {
+    textAlign: 'center',
+    fontFamily: 'OpenSans-Regular',
+    fontSize: 15,
+    color: '#95a5a6', // light grey colour
+  },
+  button: {
+    backgroundColor: '#778beb', // purple blue colour
+    padding: 10,
+    marginTop: 60,
+  },
+  buttonText: {
+    textAlign: 'center',
+    fontFamily: 'OpenSans-SemiBold',
+    fontSize: 15,
+    color: '#F6F8FA', // basically white colour
+  },
+  lineStyle: {
+    marginVertical: 20,
+    borderWidth: 0.3,
+    borderColor: '#95a5a6',
+  },
+  
+
+})
+
